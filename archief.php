03450bcb2f3c115d61891190964e133ebf85491f
fdschepper <333476@student.mboutrecht.nl>
2022-03-22 15:00:06 +0100

    :( works not (tried checking if post title is empty)

--- archief.php
+++ archief.php
@@ -33,28 +33,39 @@ echo $maxpost . ' posts in totaal';
     <br>
     <div class="row archive">
             <div class="col-12 border-25 bg-grey largepost">
-                <div class="col-6">
-                    <img class="border-25" src=
-                    "
-                    <?php 
-                    $sql = "SELECT * FROM `posts` ORDER BY id DESC LIMIT $postindex , 1";
-                    $query = mysqli_query($conn, $sql);
-                    while ($row = mysqli_fetch_array($query)) 
-                    echo $row['img'] ?>
-                    ">
-                </div>
                 <div class="col-6 displayblock">
                     <?php 
                     $sql = "SELECT * FROM `posts` ORDER BY id DESC LIMIT $postindex, 1";
                     $query = mysqli_query($conn, $sql);
                     while ($row = mysqli_fetch_array($query)) 
                     {
-                    echo '<span class="smalltxt">' . $row['redacteur'] . '</span class="smalltxt">';
-                    echo '<h2 class="m0tb">' . $row['titel'] . '</h2>';
-                    echo '<span class="m0tb">' . $row['content'] . '</span>';
+                        //
+
+                        if(empty($row['titel']) == true)
+                        {
+                            echo '<h2 class="m0tb"> post has been deleted </h2>';
+                        }
+                        else
+                        {
+                            echo '<span class="smalltxt">' . $row['redacteur'] . '</span class="smalltxt">';
+                            echo '<h2 class="m0tb">' . $row['titel'] . '</h2>';
+                            echo '<span class="m0tb">' . $row['content'] . '</span>';
+                        }
                     }
                     ?>
                 </div>
+                <div class="col-6">
+                    <img class="border-25" src=
+                    "<?php 
+                    $sql = "SELECT * FROM `posts` ORDER BY id DESC LIMIT $postindex , 1";
+                    $query = mysqli_query($conn, $sql);
+                    while ($row = mysqli_fetch_array($query)) 
+                    {
+                    echo $row['img'];
+                    }
+                    ?>">
+                </div>
+                
             </div>
         </div>
         
