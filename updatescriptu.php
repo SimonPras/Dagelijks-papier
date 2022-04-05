181ca52072e32fbcdc2ab74e8f8269842949f259
fdschepper <333476@student.mboutrecht.nl>
2022-04-05 14:27:59 +0200

    updatescript edits

--- updatescriptu.php
+++ updatescriptu.php
@@ -1,18 +1,21 @@
-<?php include("./connect_db.php") ?>
+<?php include("./connect.php") ?>
 <?php var_dump($_POST);
 
 $id = $_POST['id'];
+$fname = $_POST['fname'];
+$mname = $_POST['mname'];
+$lname = ($_POST['lname']);
 $email = $_POST['email'];
-$password = $_POST['password'];
-$active = ($_POST['active']);
 $role = $_POST['role'];
+$active = $_POST['active'];
+
 var_dump($active);
 $activeint = (int) $active;
 var_dump($activeint);
 
 echo "<br>";
-$sql = "UPDATE `register` SET `email` = '$email', `password` = '$password', `role` = '$role', `email` = '$email', `role` = '$role', `activated` = $activeint WHERE `id` = $id;";
+$sql = "UPDATE `gebruikers` SET `fname` = '$fname', `middlename` = '$mname', `lastname` = '$lname', `email` = '$email', `role` = '$role', `active` = $activeint WHERE `id` = $id;";
 mysqli_query($conn, $sql);
-header("Location: ./index.php?content=a-Users");
+header("Location: ./admind.php");
 echo $sql;
 ?>
