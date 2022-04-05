9ee5393b68bd51f88e202d24ba3244c150dc169d
fdschepper <333476@student.mboutrecht.nl>
2022-04-05 14:48:21 +0200

    edituser looks super awesom

--- edituser.php
+++ edituser.php
@@ -13,31 +13,46 @@ $record = mysqli_fetch_assoc($result); ?>
 <?php $id = $_GET['id']; ?>
 
 <?php echo $record['fname']; ?>
-<div class="block">
-<form action="./updatescriptu.php" method="post">
-<label for="">voornaam</label>
-<input value="<?php echo $record['fname']; ?>" placeholder="(vereist)" type="text" name="fname" id="fname" Required>
-<br>
-<label for="">tussenvoegsel</label>
-<input value="<?php echo $record['middlename']; ?>" placeholder="(vereist)" type="text" name="mname" id="mname" Required>
-<br>
-<label for="">achternaam</label>
-<input value="<?php echo $record['lastname']; ?>" placeholder="(vereist)" type="text" name="lname" id="lname" Required>
-<br>
-<label for="">email</label>
-<input value="<?php echo $record['email']; ?>" placeholder="(vereist)" type="text" name="email" id="email" Required>
-<br>
-<label for="">role</label>
-<input value="<?php echo $record['role']; ?>" placeholder="(vereist)" type="text" name="role" id="role" Required>
-<br>
-<label for="">activated</label>
-<input value="<?php echo $record['active']; ?>" placeholder="(vereist)" type="text" name="active" id="active" Required>
-<br>
 
-<br>
-<label for="">id to edit</label>
-<input value="<?php echo $record['id']; ?>" placeholder="(vereist)" type="text" name="id" id="id" Required>
-<br>
-
-<input class="Confirmbtn" type="submit" value="verander">
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-6">
+            <div class="block">
+            <h3>Edit user</h3>
+            <form action="./updatescriptu.php" method="post">
+                <div class="form-group">
+                    <label for="">voornaam</label>
+                    <input value="<?php echo $record['fname']; ?>" placeholder="(vereist)" type="text" name="fname" id="fname" Required>
+                    <small id="titel" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="">tussenvoegsel</label>
+                    <input value="<?php echo $record['middlename']; ?>" placeholder="" type="text" name="mname" id="mname">
+                    <small id="content" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="">achternaam</label>
+                    <input value="<?php echo $record['lastname']; ?>" placeholder="(vereist)" type="text" name="lname" id="lname" Required>
+                    <small id="foto" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="">email</label>
+                    <input value="<?php echo $record['email']; ?>" placeholder="(vereist)" type="text" name="email" id="email" Required>
+                    <small id="foto" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="">role</label>
+                    <input value="<?php echo $record['role']; ?>" placeholder="(vereist)" type="text" name="role" id="role" Required>
+                    <small id="foto" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="">activated</label>
+                    <input value="<?php echo $record['active']; ?>" placeholder="(vereist)" type="text" name="active" id="active" Required>
+                    <small id="foto" class="form-text text-muted"></small>
+                </div>
+                <input class="btm btn-danger" type="submit" value="verander">
+            </form>
+            </div>
+        </div>
+    </div>
 </div>
